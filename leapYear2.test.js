a3b26b12a49e0e48d7a250d5dffffd3a1172fc99
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-09-26 16:06:11 +0100

    added second test

--- leapYear2.test.js
+++ leapYear2.test.js
@@ -3,5 +3,8 @@ const leapYear2 = require('./leapYear2');
 describe('leapYear2', () => {
   it('returns true when given a number that is divisible by 400 eg 2000', () => {
     expect(leapYear2(2000)).toBe("true");
-  })
+  });
+  it('return true when given a number divisible by 4 but not 100 eg 2004', () => {
+    expect(leapYear(2004)).toBe("true");
+  });
 });
