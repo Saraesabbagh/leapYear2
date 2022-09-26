34d08092cc886ca258a3796b7d1db67604524f73
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-09-26 16:09:19 +0100

    added 3rd test and ran red

--- leapYear2.test.js
+++ leapYear2.test.js
@@ -7,4 +7,8 @@ describe('leapYear2', () => {
   it('return true when given a number divisible by 4 but not 100 eg 2004', () => {
     expect(leapYear2(2004)).toBe("true");
   });
+  it('returns false when give a number divisible by 100 but not 400 eg 1700', () => {
+    expect(leapYear2(1700)).toBe("false");
+  });
+
 });
